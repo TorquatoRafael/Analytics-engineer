d037403f0fb72839826cded7d2850a98caf0eeca
Rafael Torquato <rafastorquato@hotmail.com>
2025-01-03 17:41:37 +0000

    Created product dimension

--- models/intermediate/int_sales__prep_products.sql
+++ models/intermediate/int_sales__prep_products.sql
@@ -16,10 +16,9 @@ with
     ,complete_product as (
         select
              products.pk_product
-            ,productsubcategorys.fk_product_category
             ,products.name_product
-            ,productsubcategorys.name_product_subcategory
             ,productcategorys.name_product_category
+            ,productsubcategorys.name_product_subcategory
         from products 
         left join productsubcategorys on products.fk_product_subcategory = productsubcategorys.pk_product_subcategory
         left join productcategorys on productcategorys.pk_product_category = productsubcategorys.fk_product_category)
