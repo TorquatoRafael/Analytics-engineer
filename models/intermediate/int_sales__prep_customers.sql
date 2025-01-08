a900864199dbd7270409b75c2d680ab4bd6d42a9
Rafael Torquato <rafastorquato@hotmail.com>
2025-01-08 18:03:57 +0000

    We have completed the part of the test requested by the CEO, but the documentation of the Marts and Staging models is still pending.

--- models/intermediate/int_sales__prep_customers.sql
+++ models/intermediate/int_sales__prep_customers.sql
@@ -7,14 +7,30 @@ with
         select *
         from {{ ref('stg_sources__person') }})
 
+    ,store as(
+        select * 
+        from {{ ref('stg_sources__store') }}
+     )
+
     ,complete_customers as (
         select
             customers.pk_customer
-            ,person.type_person
-            ,person.first_name
-            ,person.last_name
+            ,CASE WHEN 
+                person.full_name IS NOT NULL 
+                THEN person.full_name 
+                WHEN store.store_name IS NOT NULL 
+                THEN store.store_name 
+                ELSE 'Not Found' END AS name
+            ,CASE WHEN 
+                person.full_name IS NOT NULL 
+                THEN 'Natural Person'
+                WHEN store.store_name IS NOT NULL 
+                THEN 'Legal Entity'
+                ELSE 'Not Found' END AS type_customer    
         from customers
         left join person on person.pk_person = customers.pk_customer
+        left join store on store.pk_store =  customers.pk_customer
     )
     
 select * from complete_customers
+
--- models/staging/sources/stg_sources__person.sql
+++ models/staging/sources/stg_sources__person.sql
@@ -2,9 +2,9 @@ with
      source_people as (
         select
               cast(BUSINESSENTITYID as int) as pk_person
-            , cast(PERSONTYPE as string) as type_person
-            , cast(FIRSTNAME as string) as first_name
-            , cast(LASTNAME as string) as last_name
+            ,FIRSTNAME::varchar || ' ' || LASTNAME::varchar as full_name  
+            ,cast(FIRSTNAME as string) as first_name
+            ,cast(LASTNAME as string) as last_name
         from {{ source('sources_adventure_works', 'person') }}
     )
 
