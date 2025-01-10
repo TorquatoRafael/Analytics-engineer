06c9b61ca303ec32a475ad097a190fceb59aee2e
Rafael Torquato <rafastorquato@hotmail.com>
2025-01-10 18:39:18 +0000

    Removed duplicate keys from FK_SALES_ORDER. Considered the maximum value of FK_SALES_REASON as the criterion.

--- models/intermediate/int_sales_prep_salesreason.sql
+++ models/intermediate/int_sales_prep_salesreason.sql
@@ -1,7 +1,17 @@
 with 
     sales_orders_reasons_header as (
-        select *
+        select 
+        FK_SALES_ORDER
+        ,FK_SALES_REASON
+        ,ROW_NUMBER() OVER (PARTITION BY FK_SALES_ORDER ORDER BY FK_SALES_REASON DESC) AS rn
         from {{ ref('stg_sources__salesorderheadersalesreason') }}
+    )
+    ,ranked_sales_orders_reasons as (
+        select 
+            FK_SALES_ORDER
+            ,FK_SALES_REASON
+        from sales_orders_reasons_header
+        where rn = 1
     )
 
     ,sales_orders_reasons as (
@@ -11,12 +21,10 @@ with
 
     ,complete_sales_reason as (
         select
-            
-            sales_orders_reasons_header.pk_sales_order_reason
-            ,sales_orders_reasons_header.fk_sales_order 
+            ranked_sales_orders_reasons.fk_sales_order 
             ,sales_orders_reasons.name_sales_reason
             ,sales_orders_reasons.type_sales_reason
-            from sales_orders_reasons_header
-            left join sales_orders_reasons on sales_orders_reasons_header.fk_sales_reason = sales_orders_reasons.pk_sales_reason
+            from ranked_sales_orders_reasons
+            left join sales_orders_reasons on ranked_sales_orders_reasons.fk_sales_reason = sales_orders_reasons.pk_sales_reason
     )
     select * from complete_sales_reason
