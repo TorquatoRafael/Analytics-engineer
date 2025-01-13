40cdcbc85ca7c35c835139c6c350c1ebf69ecb05
Rafael Torquato <rafastorquato@hotmail.com>
2025-01-13 13:01:45 +0000

    Category and subcategory contained null fields. Adjustment made to bring Raw Material/Maintenance Material.

--- models/intermediate/int_sales__prep_products.sql
+++ models/intermediate/int_sales__prep_products.sql
@@ -17,8 +17,14 @@ with
         select
              products.pk_product
             ,products.name_product
-            ,productcategorys.name_product_category
-            ,productsubcategorys.name_product_subcategory
+            ,CASE WHEN 
+                name_product_category IS NOT NULL 
+                THEN name_product_category
+                ELSE 'Raw Material/Maintenance Material' END AS name_product_category
+            ,CASE WHEN 
+                productsubcategorys.name_product_subcategory IS NOT NULL 
+                THEN productsubcategorys.name_product_subcategory
+                ELSE 'Raw Material/Maintenance Material' END AS name_product_subcategory
         from products 
         left join productsubcategorys on products.fk_product_subcategory = productsubcategorys.pk_product_subcategory
         left join productcategorys on productcategorys.pk_product_category = productsubcategorys.fk_product_category)
